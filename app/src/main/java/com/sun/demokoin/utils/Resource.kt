51461f12b325f6d7156f5b0cc88562176b8aa800
phong10101998 <phongphongphong10101998@gmail.com>
2021-04-08 16:59:38 +0700

    init view

---
+++ app/src/main/java/com/sun/demokoin/utils/Resource.kt
@@ -0,0 +1,18 @@
+package com.sun.demokoin.utils
+
+import com.sun.demokoin.utils.Status.ERROR
+import com.sun.demokoin.utils.Status.LOADING
+import com.sun.demokoin.utils.Status.SUCCESS
+
+data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+    companion object {
+        fun <T> success(data: T): Resource<T> =
+            Resource(status = SUCCESS, data = data, message = null)
+
+        fun <T> error(data: T?, message: String): Resource<T> =
+            Resource(status = ERROR, data = data, message = message)
+
+        fun <T> loading(data: T?): Resource<T> =
+            Resource(status = LOADING, data = data, message = null)
+    }
+}
